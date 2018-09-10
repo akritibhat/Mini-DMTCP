03bc8fb98aa730b6df5c1c8396e38f34fa540bb9
Akriti <akriti.bhat28@gmail.com>
2018-09-10 12:23:08 -0400

    formatting

--- libckpt.c
+++ libckpt.c
@@ -10,162 +10,156 @@
 
 int flag;
 void sig_handler(int signum);
-void __attribute__((constructor))myconstructor();
-void myconstructor(){
-		signal(SIGUSR2, sig_handler);
-		//signal(SIGINT, sig_handler);    //signal handler
+void __attribute__((constructor)) myconstructor();
+void myconstructor() {
+  signal(SIGUSR2, sig_handler);
+  //signal(SIGINT, sig_handler);    //signal handler
 }
 
+void sig_handler(int signum) {
 
-void sig_handler(int signum){
+  ucontext_t context;
 
-	ucontext_t context;
+  int pid = getpid();
 
-	int pid=getpid();
+  getcontext( & context); //saving context
+  if (getpid() != pid) { //setting pid as a flag
+    printf("i am back in libckpt!!!");
+    return;
+  }
 
-	getcontext(&context); //saving context
-	if(getpid()!=pid){ //setting pid as a flag
-		printf("i am back in libckpt!!!");
-			return ;
-	}
+  char addrstr[80];
+  int count = 1;
+  int co = 0;
+  char line[400];
+  int tempChar;
+  unsigned int tempCharIdx = 0 U;
+  strcpy(addrstr, "/proc/self");
+  strcat(addrstr, "/maps");
 
-		char addrstr[80];
-		int count=1;
-		 int co=0;
-		char line[400];
-	  int tempChar;
-	  unsigned int tempCharIdx = 0U;
-		strcpy(addrstr, "/proc/self");
-		strcat(addrstr,"/maps");
-	  
-	  int filep,fileOutput;
-	  char c;
-	  filep= open(addrstr,O_RDONLY,0600);
+  int filep, fileOutput;
+  char c;
+  filep = open(addrstr, O_RDONLY, 0600);
 
-while(read(filep, &c, 1)>0){
-if (c == '\n')
-				 count = count + 1;
-}
-close(filep);
+  while (read(filep, & c, 1) > 0) {
+    if (c == '\n')
+      count = count + 1;
+  }
+  close(filep);
 
-filep= open(addrstr,O_RDONLY,0600); //opening proc/self/maps
+  filep = open(addrstr, O_RDONLY, 0600); //opening proc/self/maps
 
-remove("myckpt");
+  remove("myckpt");
 
-if((fileOutput =open("myckpt", O_CREAT|O_WRONLY,0600))<0)  //creating mychpkt image
-		{
-						 printf("An error occured.");
-		}
+  if ((fileOutput = open("myckpt", O_CREAT | O_WRONLY, 0600)) < 0) //creating mychpkt image
+  {
+    printf("An error occured.");
+  }
 
-memRegion *memArray=(memRegion*)malloc(count*sizeof(memRegion));
+  memRegion * memArray = (memRegion * ) malloc(count * sizeof(memRegion));
 
-while(read(filep, &tempChar, 1)>0){
-					if (tempChar == '\n') {
-					            line[tempCharIdx] = '\0';
-					            tempCharIdx = 0U;
+  while (read(filep, & tempChar, 1) > 0) {
+    if (tempChar == '\n') {
+      line[tempCharIdx] = '\0';
+      tempCharIdx = 0 U;
 
-					 	  char *startAddr;
-					  	char *endAddr;
-						  int posofdash;
-						  int posofspace;
-						  char linetemp[200];
+      char * startAddr;
+      char * endAddr;
+      int posofdash;
+      int posofspace;
+      char linetemp[200];
 
-							strcpy(linetemp,line);
-							startAddr=strtok(line,"-");
+      strcpy(linetemp, line);
+      startAddr = strtok(line, "-");
 
-							//printf("\n%i: %s-",co,startAddr);
+      //printf("\n%i: %s-",co,startAddr);
 
-							strcpy(memArray[co].startaddr,startAddr);
-							posofdash = strlen(startAddr) +1;
-							strcpy(line,linetemp);
+      strcpy(memArray[co].startaddr, startAddr);
+      posofdash = strlen(startAddr) + 1;
+      strcpy(line, linetemp);
 
-							int lenofline=strlen(line);
+      int lenofline = strlen(line);
 
-							strncpy(linetemp,line+posofdash,lenofline);
-							endAddr=strtok(linetemp," ");
+      strncpy(linetemp, line + posofdash, lenofline);
+      endAddr = strtok(linetemp, " ");
 
-							posofspace= posofdash+strlen(endAddr) +1;
-							char lineper[4];
+      posofspace = posofdash + strlen(endAddr) + 1;
+      char lineper[4];
 
-							lenofline=posofspace+4;
+      lenofline = posofspace + 4;
 
-							//printf("%s",endAddr);
+      //printf("%s",endAddr);
 
-							strcpy(memArray[co].endaddr,endAddr);
+      strcpy(memArray[co].endaddr, endAddr);
 
-							//printf("\n %s %s",memArray[co].startaddr, memArray[co].endaddr);
-							//memArray[co].size = difference(memArray[co].endaddr,memArray[co].startaddr) ;
-							int diff= difference(memArray[co].endaddr,memArray[co].startaddr);
-							//printf("\n %i size:", diff);
-							memArray[co].size=diff;
-						 printf("\n after copy size  %i",memArray[co].size);
+      //printf("\n %s %s",memArray[co].startaddr, memArray[co].endaddr);
+      //memArray[co].size = difference(memArray[co].endaddr,memArray[co].startaddr) ;
+      int diff = difference(memArray[co].endaddr, memArray[co].startaddr);
+      //printf("\n %i size:", diff);
+      memArray[co].size = diff;
+      printf("\n after copy size  %i", memArray[co].size);
 
-							strcpy(linetemp,line+posofspace);
+      strcpy(linetemp, line + posofspace);
 
-							strncpy(lineper,linetemp,4);
+      strncpy(lineper, linetemp, 4);
 
-							char *token;
-						  	token=strchr (lineper, 'r');
+      char * token;
+      token = strchr(lineper, 'r');
 
-							if (token!=NULL){
-								memArray[co].isReadable=1;
-							}
+      if (token != NULL) {
+        memArray[co].isReadable = 1;
+      }
 
-							token=strchr (lineper, 'w');
+      token = strchr(lineper, 'w');
 
-							if (token!=NULL){
-								memArray[co].isWritable=1;
-							}
+      if (token != NULL) {
+        memArray[co].isWritable = 1;
+      }
 
-							token=strchr (lineper, 'x');
+      token = strchr(lineper, 'x');
 
-							if (token!=NULL){
-								memArray[co].isExecutable=1;
-							}
+      if (token != NULL) {
+        memArray[co].isExecutable = 1;
+      }
 
-							co++;
-										continue;
-										}
-										        else
-										            line[tempCharIdx++] = (char)tempChar;
-										    	}
+      co++;
+      continue;
+    } else
+      line[tempCharIdx++] = (char) tempChar;
+  }
 
-													for(int i=0;i<count;i++){
-														//printf("\n %s %s",memArray[i].startaddr, memArray[i].endaddr);
-													}
+  for (int i = 0; i < count; i++) {
+    //printf("\n %s %s",memArray[i].startaddr, memArray[i].endaddr);
+  }
 
+  write(fileOutput, & context, sizeof(ucontext_t)); //writing ucontext to file
 
-													 write(fileOutput,&context,sizeof(ucontext_t)); //writing ucontext to file
+  if (write(fileOutput, & count, sizeof(int)) != sizeof(int)) {
+    printf("could not write to file");
+  }
 
-													 if( write(fileOutput,&count,sizeof(int))!= sizeof(int))
-													 {
-														 printf("could not write to file");
-													 }
+  for (int i = 0; i < count; i++) {
+    printf("\n %s %s ", memArray[i].startaddr, memArray[i].endaddr); //writing data of proc/self/maps to fle
+    if (write(fileOutput, & memArray[i], sizeof(memRegion)) != sizeof(memRegion)) {
+      printf("erro copying mem address");
+    }
+  }
 
-													 for(int i=0;i<count;i++){
-														 printf("\n %s %s ",memArray[i].startaddr, memArray[i].endaddr); //writing data of proc/self/maps to fle
- 													if(write(fileOutput, &memArray[i], sizeof(memRegion))!=sizeof(memRegion)){
-														printf("erro copying mem address");
-													}
- 													}
+  for (int i = 0; i < count - 1; i++) {
+    printf("\n copying memory %s %s", memArray[i].startaddr, memArray[i].endaddr);
+    if (memArray[i].isReadable) {
+      long long hexa = getDecimalValue(memArray[i].startaddr);
+      printf("%llx ---- ", hexa);
+      //writing data within memory segments to the file
+      if (write(fileOutput, (void * ) hexa, memArray[i].size) != memArray[i].size) {
+        printf("error copying memory at %s\n", memArray[i].startaddr);
+      }
 
-													for(int i=0;i<count-1;i++){
-														printf("\n copying memory %s %s",memArray[i].startaddr, memArray[i].endaddr);
-												    if(memArray[i].isReadable){
-												    long long hexa= getDecimalValue(memArray[i].startaddr);
-														printf("%llx ---- ",hexa );
-														//writing data within memory segments to the file
-												 if(write(fileOutput,(void *)hexa,memArray[i].size)!=memArray[i].size){
-												 printf("error copying memory at %s\n",memArray[i].startaddr);
-													}
+    } else {
+      printf("the memory is non readable\n");
+    }
+  }
 
-												  }
-													else
-									    		{
-									    			printf("the memory is non readable\n");
-									    		}
-												  }
-
-													close(fileOutput);
-													exit(0);
+  close(fileOutput);
+  exit(0);
 }
--- myRestart.c
+++ myRestart.c
@@ -17,122 +17,112 @@ char image[30];
 char stackstartaddr[20];
 char stackendaddr[20];
 
-int main(int argc, char* argv[])
-{
-   strcpy(image,argv[1]);
-  printf("the pid is : %d\n",getpid() );
+int main(int argc, char * argv[]) {
+  strcpy(image, argv[1]);
+  printf("the pid is : %d\n", getpid());
 
-    restore();
-    void* stackAddr=(void *)0x5300000;
-    void* stack_ptr=(void *)0x5301000;
-    void* addr= mmap(stackAddr, 4096, PROT_EXEC|PROT_WRITE|PROT_READ, MAP_GROWSDOWN|MAP_PRIVATE|MAP_ANONYMOUS,-1, 0); //mmap to allocate memory
-       if(addr==MAP_FAILED)
-       {
-           printf("memory mapping failed..");
-           exit(1);
-       }
+  restore();
+  void * stackAddr = (void * ) 0x5300000;
+  void * stack_ptr = (void * ) 0x5301000;
+  void * addr = mmap(stackAddr, 4096, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_GROWSDOWN | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); //mmap to allocate memory
+  if (addr == MAP_FAILED) {
+    printf("memory mapping failed..");
+    exit(1);
+  }
 
-   asm volatile("mov %0,%%rsp;" : : "g" (stack_ptr) : "memory"); //moving stack pointer to the new memory created
-   restoreFile();
+  asm volatile("mov %0,%%rsp;":: "g" (stack_ptr): "memory"); //moving stack pointer to the new memory created
+  restoreFile();
 }
 
-
-
-void restore()
-{
-    int len_line;
-    int fileinput;
-    char buffer[200]="";
-    int i = 0;
-    char *fileName="/proc/self/maps"; //reading proc/self/maps to get stack address
+void restore() {
+  int len_line;
+  int fileinput;
+  char buffer[200] = "";
+  int i = 0;
+  char * fileName = "/proc/self/maps"; //reading proc/self/maps to get stack address
   if ((fileinput = open(fileName, O_RDONLY, 0)) != -1) { //if 1
-  while (read(fileinput, &buffer[i], 1) > 0) {
-  if (buffer[i] == '\n') {
-     i=0;
-     char *startAddr;
-                char *endAddr;
-            int posofdash;
-            int posofspace;
+    while (read(fileinput, & buffer[i], 1) > 0) {
+      if (buffer[i] == '\n') {
+        i = 0;
+        char * startAddr;
+        char * endAddr;
+        int posofdash;
+        int posofspace;
 
-                char tempString[200];
-                char *key = "stack";
-                if(strstr(buffer, key) != NULL) { //finding the string with keyword stack
-                    strcpy(tempString,buffer);
+        char tempString[200];
+        char * key = "stack";
+        if (strstr(buffer, key) != NULL) { //finding the string with keyword stack
+          strcpy(tempString, buffer);
 
+          startAddr = strtok(buffer, "-");
+          strcpy(stackstartaddr, startAddr);
 
-                      startAddr=strtok(buffer,"-");
-                    strcpy(stackstartaddr,startAddr);
+          posofdash = strlen(startAddr) + 1;
+          strcpy(buffer, tempString);
+          len_line = strlen(buffer);
 
-                           posofdash = strlen(startAddr)+1;
-                   strcpy(buffer,tempString);
-                   len_line=strlen(buffer);
-
-                   strncpy(tempString,buffer+posofdash,len_line);
-                           endAddr=strtok(tempString," ");
-                         strcpy(stackendaddr,endAddr);
-                                    break;
-                }
-              continue;
-              }
-        i++;
-    }
-
-    }
-    else
-    {
-      printf("\nError: file not found");
-    }
-
-close(fileinput);
-}
-
-void stack_unmap(){
-int stack_size=difference(stackendaddr,stackstartaddr) ;
-long long hex= getDecimalValue(stackstartaddr);
-void *poi = (void *)hex;
-munmap(poi, stack_size);
-}
-
-void restoreFile()
-{
-     stack_unmap();
-    int fileInput= open(image,O_RDONLY,0600);
-    int count;
-   read(fileInput,&myContext, sizeof(ucontext_t));
-    read(fileInput,&count,sizeof(int));
-    printf("%i",count);
-    memRegion mem[count];
-    for(int i=0;i<count;i++){
-    read(fileInput,&mem[i], sizeof(memRegion));
-    printf("\n%s",mem[i].startaddr);
-               printf(" - %s",mem[i].endaddr);
-                 printf("  %d",mem[i].isReadable);
-                 printf("  %d",mem[i].size);
-                  printf("  %d",i);
+          strncpy(tempString, buffer + posofdash, len_line);
+          endAddr = strtok(tempString, " ");
+          strcpy(stackendaddr, endAddr);
+          break;
+        }
+        continue;
       }
-
-         for(int i=0;i<count-1;i++){
-					//printf("\nmmap at location %d %s size: %d r:%d", i, mem[i].startaddr, mem[i].size, mem[i].isReadable);
-						long long hex= getDecimalValue(mem[i].startaddr);
-            void* stackAddr=(void *)hex;
-						void * addr= mmap(stackAddr, mem[i].size, PROT_EXEC|PROT_WRITE|PROT_READ,MAP_FIXED|MAP_GROWSDOWN|MAP_PRIVATE|MAP_ANONYMOUS,-1, 0);
-            if(addr==MAP_FAILED)
-                 {
-                     printf("fail");
-                 }
-			//		printf("size \n %d",read(fileInput,(void *)hex,mem[i].size));
-		}
-
-    for(int v=0;v<count-1;v++){
-      printf("\nin readable");
-    				if(mem[v].isReadable){
-           printf("\nat location*%d*%s*size:*%d*r:*%d*", v, mem[v].startaddr, mem[v].size, mem[v].isReadable);
-    				   long long starinhexa=getDecimalValue(mem[v].startaddr);
-      					 int address =read(fileInput,(void *)starinhexa,mem[v].size);
-    						printf(" index:%d:  %d",v,address);
-
-    				}
+      i++;
     }
-    setcontext(&myContext);
-    exit(0);
+
+  } else {
+    printf("\nError: file not found");
+  }
+
+  close(fileinput);
+}
+
+void stack_unmap() {
+  int stack_size = difference(stackendaddr, stackstartaddr);
+  long long hex = getDecimalValue(stackstartaddr);
+  void * poi = (void * ) hex;
+  munmap(poi, stack_size);
+}
+
+void restoreFile() {
+  stack_unmap();
+  int fileInput = open(image, O_RDONLY, 0600);
+  int count;
+  read(fileInput, & myContext, sizeof(ucontext_t));
+  read(fileInput, & count, sizeof(int));
+  printf("%i", count);
+  memRegion mem[count];
+  for (int i = 0; i < count; i++) {
+    read(fileInput, & mem[i], sizeof(memRegion));
+    printf("\n%s", mem[i].startaddr);
+    printf(" - %s", mem[i].endaddr);
+    printf("  %d", mem[i].isReadable);
+    printf("  %d", mem[i].size);
+    printf("  %d", i);
+  }
+
+  for (int i = 0; i < count - 1; i++) {
+    //printf("\nmmap at location %d %s size: %d r:%d", i, mem[i].startaddr, mem[i].size, mem[i].isReadable);
+    long long hex = getDecimalValue(mem[i].startaddr);
+    void * stackAddr = (void * ) hex;
+    void * addr = mmap(stackAddr, mem[i].size, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_FIXED | MAP_GROWSDOWN | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (addr == MAP_FAILED) {
+      printf("fail");
+    }
+    //		printf("size \n %d",read(fileInput,(void *)hex,mem[i].size));
+  }
+
+  for (int v = 0; v < count - 1; v++) {
+    printf("\nin readable");
+    if (mem[v].isReadable) {
+      printf("\nat location*%d*%s*size:*%d*r:*%d*", v, mem[v].startaddr, mem[v].size, mem[v].isReadable);
+      long long starinhexa = getDecimalValue(mem[v].startaddr);
+      int address = read(fileInput, (void * ) starinhexa, mem[v].size);
+      printf(" index:%d:  %d", v, address);
+
+    }
+  }
+  setcontext( & myContext);
+  exit(0);
 }
